363b1ca8025e8ae5d37f20860ad5f09981c6b758
HARIHARAN D <dhariharan38@gmail.com>
2026-02-20 11:22:05 +0530

    Add UC3PalindromeCheckerApp.java file

---
+++ src/UC3PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class UC2PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        boolean isPalindrome = true;
+
+        // Compare from both ends
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
