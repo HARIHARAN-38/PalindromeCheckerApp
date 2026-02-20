3e5432697bfdf6a5034fdfb7bc00f6cb309a714a
HARIHARAN-38 <dhariharan38@gmail.com>
2026-02-20 11:17:22 +0530

    UC3 Algorithm done

---
+++ src/UC3PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+public class UC3PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        boolean isPalindrome = true;
+
+        // Compare from both ends
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
